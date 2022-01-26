cf595a073787a101249539a8c530fede8402fc60
VictoriaCardos <vvjcerque@hotmail.com>
2022-01-26 19:42:20 -0300

    feat: children

--- src/components/containerBox/index.jsx
+++ src/components/containerBox/index.jsx
@@ -1,9 +1,10 @@
 import { Cont } from './style'
 
-const Container = () => {
+const Container = props => {
   return (
     <Cont>
       <h1>Busca CEP: </h1>
+      {props.children}
     </Cont>
   )
 }
