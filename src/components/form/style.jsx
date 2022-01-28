149b3340d441091009b6c2f410440dc7f8e10be4
VictoriaCardos <vvjcerque@hotmail.com>
2022-01-27 21:39:13 -0300

    style: adjust button and input

--- src/components/form/style.jsx
+++ src/components/form/style.jsx
@@ -1,2 +1,33 @@
 import styled from 'styled-components'
-export const Content = styled.form``
+export const Content = styled.form`
+  margin-bottom: 10%;
+
+  input {
+    border: none;
+    height: 40px;
+    width: 260px;
+  }
+  input:focus,
+  select:focus {
+    box-shadow: 0 0 0 0;
+    border: 0 none;
+    outline: 0;
+    border-bottom: 4px solid var(--more-dark);
+  }
+  button {
+    background-color: transparent;
+    border: none;
+    transition-duration: 0.4s;
+    position: relative;
+    top: 12px;
+    cursor: pointer;
+  }
+
+  button:hover {
+    background-color: var(--button-search);
+  }
+
+  img {
+    width: 35px;
+  }
+`
