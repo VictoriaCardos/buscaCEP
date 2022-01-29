376e1c89121a2f74a1e3e1af09274f495b0e3746
VictoriaCardos <vvjcerque@hotmail.com>
2022-01-28 21:46:19 -0300

    feat: add complemento and refactoring

--- src/components/dataCep/index.jsx
+++ src/components/dataCep/index.jsx
@@ -7,33 +7,31 @@ const DataCep = () => {
   return (
     <DataCEP>
       {ctx.cep?.cep ? (
-        <h2>
-          <strong>CEP:</strong> {ctx.cep?.cep}
-        </h2>
-      ) : undefined}
-      {ctx.cep?.cep ? (
-        <h2>
-          <strong>Logradouro: </strong>
-          {ctx.cep?.logradouro}
-        </h2>
-      ) : undefined}
-      {ctx.cep?.cep ? (
-        <h2>
-          <strong>Bairro: </strong>
-          {ctx.cep?.bairro}
-        </h2>
-      ) : undefined}
-      {ctx.cep?.cep ? (
-        <h2>
-          <strong>Localidade: </strong>
-          {ctx.cep?.localidade}
-        </h2>
-      ) : undefined}
-      {ctx.cep?.cep ? (
-        <h2>
-          <strong>UF: </strong>
-          {ctx.cep?.uf}
-        </h2>
+        <>
+          <h2>
+            <strong>CEP:</strong> {ctx.cep?.cep}
+          </h2>
+          <h2>
+            <strong>Logradouro: </strong>
+            {ctx.cep?.logradouro}
+          </h2>
+          <h2>
+            <strong>Bairro: </strong>
+            {ctx.cep?.bairro}
+          </h2>
+          <h2>
+            <strong>Complemento: </strong>
+            {ctx.cep?.complemento}
+          </h2>
+          <h2>
+            <strong>Localidade: </strong>
+            {ctx.cep?.localidade}
+          </h2>
+          <h2>
+            <strong>UF: </strong>
+            {ctx.cep?.uf}
+          </h2>
+        </>
       ) : undefined}
     </DataCEP>
   )
